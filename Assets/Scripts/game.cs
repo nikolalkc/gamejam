542638d3558b4fb3c88e0cfa0914a381e4b07cf8
Nikola Lukic <nikolalkc@hotmail.com>
2015-06-07 13:40:56 +0200

    Shift i Alt menjaju vreme

--- Assets/Scripts/game.cs
+++ Assets/Scripts/game.cs
@@ -7,4 +7,24 @@ public class game : MonoBehaviour {
 	public static float globalSnakeSpeed = 3;
 	public static float globalSpawnSpeed = 0.7f;
 
+	public	int timeScaleSpeedUp = 5;
+	public  int timeScaleSpeedDown = 7;
+	void Update() {
+
+		if (Input.GetKeyDown (KeyCode.RightShift)) {
+			Time.timeScale *= timeScaleSpeedUp;
+		}
+		if (Input.GetKeyUp (KeyCode.RightShift)) {
+			Time.timeScale /= timeScaleSpeedUp;
+		}
+
+		if (Input.GetKeyDown (KeyCode.RightAlt)) {
+			Time.timeScale /= timeScaleSpeedDown;
+		}
+		if (Input.GetKeyUp (KeyCode.RightAlt)) {
+			Time.timeScale *= timeScaleSpeedDown;
+		}
+
+	}
+
 }
